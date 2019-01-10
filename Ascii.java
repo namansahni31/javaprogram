d4b4e2292c6dfa301e18d588139da5a02b448dc7
naman <naman18csu136@ncuindia.edu>
2019-01-10 22:23:45 +0530

    DisplayHello is updated

---
+++ Ascii.java
@@ -0,0 +1,11 @@
+public class Ascii
+{
+	public static void main(String[] args)
+	{
+	for(i=0;ii<=127;i++)
+	{
+	 sysytem.out.printf("%d : %c \n ",i,i);
+	}
+	}
+	}
+}
---
+++ Dynamic.java
@@ -0,0 +1,11 @@
+class Dynamic
+{
+  public static void main(String args[]) 
+  {
+    int num; // this declares a variable called num
+    num = 100; // this assigns num the value 100
+   int num1 = num/2;
+    System.out.println("This is num: " + num);
+    System.out.println(num1);
+  }
+}
